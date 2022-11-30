d6fe49eb4deb97d0102d3031a38abf9e6b2486b5
Abdullah Mosa <amosa@ogoul.com>
2022-11-30 03:10:50 +0300

    add plugin files

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Elementor-Video-Slider
+# Elementor Video Slider Plugin
 Custom Elementor video slider with popup
+
+![license](https://img.shields.io/badge/license-GNU-blue.svg)
+
+> Elevate Your Skills & Grow Professionally.
+
+If you’re an eCommerce store or a service provider, videos of products and services are a great way to pitch offers and impress viewers. If you’re a training provider or coach, video is equally, if not more important.
+
+So it pays to showcase videos at their best on your website.
+
+Displaying videos on your website and ensuring visitors can view them is more than just another item on a checklist.
+
+```
+You can change this plugin to your needs.
+```
+
+Screenshots :
+
+![](screens/001.png)
+
+![](screens/002.png)
+
+![](screens/003.png)
+
+
---
+++ ah-slider/slider-widget.php
@@ -0,0 +1,134 @@
+<?php
+namespace Elementor;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+use WP_Query;
+
+class SliderWidget extends Widget_Base {
+
+    public function get_name() {
+        return 'slider-widget';
+    }
+
+    public function get_title() {
+        return 'Video Slider Widget';
+    }
+
+    public function get_icon() {
+        return 'eicon-photo-library';
+    }
+
+    public function get_keywords() {
+        return ['ah', 'video', 'slider'];
+    }
+
+    public function get_categories() {
+        return [ 'ah-category' ];
+    }
+
+    protected function _register_controls() {
+
+        $this->start_controls_section(
+            'blur_slides',
+            [
+                'label' => __( 'Slides', 'elementor' ),
+            ]
+        );
+
+        $this->add_control( 'ah-slides', [
+            'label' => __( 'Add your slides', 'my-listing' ),
+            'type' => Controls_Manager::REPEATER,
+            'fields' =>
+                [
+                    [
+                        'name' => 'slide-img',
+                        'label' => __( 'Image', 'ah-theme' ),
+                        'type' => \Elementor\Controls_Manager::MEDIA,
+                        'options' => [],
+                    ],
+                    [
+                        'name'        => 'title',
+                        'label'       => __( 'Title', 'ah-theme' ),
+                        'type'        => Controls_Manager::TEXT,
+                        'default'     => '',
+                        'label_block' => true,
+                    ],
+                    [
+                        'name'        => 'description',
+                        'label'       => __( 'Description', 'ah-theme' ),
+                        'type'        => Controls_Manager::TEXTAREA,
+                        'default'     => '',
+                        'label_block' => true,
+                    ],
+                    [
+                        'name'        => 'url',
+                        'label'       => __( 'Video URL', 'ah-theme' ),
+                        'type'        => Controls_Manager::URL,
+                        'default'   => [
+                            'url'   => ''
+                        ],
+                        'label_block' => true,
+                    ],
+                ],
+            'default' => [],
+        ]);
+
+        $this->end_controls_section();
+    }
+
+    protected function render() {
+        $settings = $this->get_settings_for_display();
+        if( !empty($settings['ah-slides']) ): ?>
+        <!-- Swiper -->
+        <div class="swiper mySwiper cu-slider-sw videoswiper">
+            <div class="swiper-wrapper">
+                <?php foreach ( $settings['ah-slides'] as $k => $slide ): ?>
+                <div class="swiper-slide">
+                    <div class="video-side-cu">
+                        <a class="popup-video" href="<?=$slide['url']['url']?>">
+                            <img class="main-slider-img-cu" src="<?=$slide['slide-img']['url']?>" alt="">
+                            <img class="play-icon-cu" height="85" src="<?=plugins_url('assets/images/play-button.png', __FILE__)?>" alt="<?=$slide['title']?>">
+                        </a>
+                    </div>
+                    <div class="text-side-cu">
+                        <h3 class="slider-header-cu avgu"><?=$slide['title']?></h3>
+                        <p class="glamode"><?=$slide['description']?></p>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+            </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        </div>
+        <?php endif; ?>
+
+        <script>
+            jQuery(document).ready(function($) {
+                new Swiper(".mySwiper", {
+                    slidesPerView: 1,
+                    spaceBetween: 0,
+                    navigation: {
+                        nextEl: ".swiper-button-next",
+                        prevEl: ".swiper-button-prev",
+                    },
+                });
+                $('.videoswiper a.popup-video').each(function(index, element) {
+                    let href = $(element).attr('href');
+                    $(element).magnificPopup({
+                        items: {src: href},
+                        type: 'iframe'
+                    });
+                });
+            });
+        </script>
+
+        <?php
+    }
+
+    protected function _content_template() {
+
+    }
+}
